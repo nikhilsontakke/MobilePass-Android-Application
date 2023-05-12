1a2758e13feafd5b3f7dbf64e16ea60b2af69bae
Rastosh <shivansh.rastogi19@vit.edu>
2023-05-12 14:51:50 +0530

    Login Activity & Page added

---
+++ app/src/main/java/com/example/mobilepass/loginActivity.java
@@ -0,0 +1,28 @@
+package com.example.mobilepass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class loginActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        button = findViewById(R.id.cirLoginButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                }
+        });
+    }
+}
